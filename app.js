2ef6f62fc0794cbbf4e634a6d563daee85ac109a
rodrigofer89 <rodrigofer89@hotmail.com>
2023-04-05 02:13:59 -0300

    Voltando o arquivo para versão inicial

--- app.js
+++ app.js
@@ -1,3 +1,1 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Aplicão está se conectando banco de dados")
-console.log("Adicionando mensagem")
+console.log("Rodando o sistema de gerenciamento de jogos"
