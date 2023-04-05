877a655ae572657767603c5639ff92cfda461bf1
rodrigofer89 <rodrigofer89@hotmail.com>
2023-04-05 02:00:04 -0300

    Adicionando nova mensagem

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Aplicão está se conectando anco de dados")
-console.log("testando git")
+console.log("Aplicão está se conectando banco de dados")
+console.log("Adicionando mensagem")
